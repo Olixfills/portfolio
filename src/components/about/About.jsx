930bce2b536450cb032f038c49f04bd42cff47ae
Oni Olayiwola <shasha@pop-os.localdomain>
2022-05-24 19:41:00 +0100

    started nav bar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./about.css";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>A little about me..</h5>
+      <h2>Let's meet Ola</h2>
+      <div className="container about__container">
+        <div className="about__img"></div>
+        <div className="about__desc"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
